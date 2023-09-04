7757cab1447ac95671eede1e228f15504b5c5cfd
cpp_programs <saitejadhondi34111@gmail.com>
2023-09-04 21:54:51 +0530

    pattern9 completed

---
+++ Patterns/pattern9.cpp
@@ -0,0 +1,55 @@
+/* Input Format: N = 6
+Result:   
+     *
+    ***
+   ***** 
+  *******
+ *********
+***********  
+***********
+ *********
+  *******
+   ***** 
+    ***    
+     *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void pattern(int n)
+{
+    int i,j,k,l;
+    for(i=1;i<=n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            cout<<" ";
+        }
+        
+        for(k=1;k<=2*i-1;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(i=n;i>=1;i--)
+    {
+        for(j=n;j>i;j--)
+        {
+            cout<<" ";
+        }
+        for(k=1;k<=2*i-1;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
+}
